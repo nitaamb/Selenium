09bcc9a31be01ee7ab7b373e99cd3025706d9ada
nitaamb <43382470+nitaamb@users.noreply.github.com>
2022-07-27 22:55:08 +0700

    first commit

--- operation.py
+++
@@ -1,8 +0,0 @@
-#Buatlah sebuah program yang dapat menentukan suatu variabel apakah merupakan bilangan genap atau ganjil.
-
-angka = 5
-
-if angka%2 == 0:
-    print("Genap")
-else:
-    print("Ganjil")
